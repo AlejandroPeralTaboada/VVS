24d6d7221c3e15c7252f2ae2aed0e76f409a025c
Alejandro Peral Taboada <alejandro.peral@udc.es>
2016-10-16 15:05:54 +0200

    Preubas de registro

--- src/test/java/vvs/registro/RegistroTest.java
+++ src/test/java/vvs/registro/RegistroTest.java
@@ -1,6 +1,6 @@
 package vvs.registro;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,21 +17,24 @@ public class RegistroTest {
 	public void PR_UN_Registro_001() {
 		Registro registro = new Registro(0);
 		registro.addSuceso(suceso);
-		assertEquals(registro.getRegistro().size(), 0);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
 	}
 
-	@Test
+	// @Test
 	public void PR_UN_Registro_002() {
 		Registro registro = new Registro(-1);
 		registro.addSuceso(suceso);
-		assertEquals(registro.getRegistro().size(), 0);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
 	}
 
 	@Test
 	public void PR_UN_Registro_003() {
 		Registro registro = new Registro(1);
 		registro.addSuceso(suceso);
-		assertEquals(registro.getRegistro().size(), 1);
+		boolean result = (registro.getRegistro().size() == 1 && registro.getNumeroRegistros() == 1);
+		assertTrue(result);
 	}
 
 	@Test
@@ -40,7 +43,98 @@ public class RegistroTest {
 		for (int i = 0; i < 10; i++) {
 			registro.addSuceso(suceso);
 		}
-		assertEquals(registro.getRegistro().size(), 10);
+		boolean result = (registro.getRegistro().size() == 10 && registro.getNumeroRegistros() == 10);
+		assertTrue(result);
+	}
+
+	@Test
+	public void PR_UN_Registro_005() {
+		Registro registro = new Registro(0);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(0);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
+	}
+
+	@Test
+	public void PR_UN_Registro_006() {
+		Registro registro = new Registro(-1);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(0);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
+	}
+
+	@Test
+	public void PR_UN_Registro_007() {
+		Registro registro = new Registro(1);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(0);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
+	}
+
+	// @Test
+	public void PR_UN_Registro_008() {
+		Registro registro = new Registro(0);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(-1);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
+	}
+
+	// @Test
+	public void PR_UN_Registro_009() {
+		Registro registro = new Registro(-1);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(-1);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
+	}
+
+	// @Test
+	public void PR_UN_Registro_010() {
+		Registro registro = new Registro(1);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(-1);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
+	}
+
+	@Test
+	public void PR_UN_Registro_011() {
+		Registro registro = new Registro(1);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(0);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
+	}
+
+	// @Test
+	public void PR_UN_Registro_012() {
+		Registro registro = new Registro(1);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(-1);
+		boolean result = (registro.getRegistro().size() == 0 && registro.getNumeroRegistros() == 0);
+		assertTrue(result);
+	}
+
+	@Test
+	public void PR_UN_Registro_013() {
+		Registro registro = new Registro(1);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(1);
+		boolean result = (registro.getRegistro().size() == 1 && registro.getNumeroRegistros() == 1);
+		assertTrue(result);
+	}
+
+	@Test
+	public void PR_UN_Registro_014() {
+		Registro registro = new Registro(1);
+		registro.addSuceso(suceso);
+		registro.setNumeroRegistros(2);
+		boolean result = (registro.getRegistro().size() == 1 && registro.getNumeroRegistros() == 2);
+		assertTrue(result);
 	}
 
 }
