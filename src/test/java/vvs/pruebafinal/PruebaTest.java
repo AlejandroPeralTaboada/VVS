9cfe98a3787caceb48246c69febef82fe496842f
Alejandro Peral <alejandro.peral@udc.es>
2016-10-27 16:10:06 +0200

    Añade cobertura

---
+++ src/test/java/vvs/pruebafinal/PruebaTest.java
@@ -0,0 +1,14 @@
+package vvs.pruebafinal;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PruebaTest {
+
+  @Test
+  public void test() {
+    Prueba.main(null);
+    assertTrue(true);
+  }
+}
