2714990679481844d6712b9c65adf38596b9c317
Alejandro Peral Taboada <alejandro.peral@udc.es>
2016-12-06 17:44:16 +0100

    errores de inicio

--- src/test/java/vvs/AutoEstado/EstadosTest.java
+++ src/test/java/vvs/AutoEstado/EstadosTest.java
@@ -22,90 +22,93 @@ import vvs.piscinas.PiscinaImp;
 
 public class EstadosTest extends ExecutionContext implements Estados {
 
-  public final static Path MODEL_PATH = Paths.get("vvs/AutoEstado/Estados.graphml");
+	public final static Path MODEL_PATH = Paths.get("vvs/AutoEstado/Estados.graphml");
 
-  private Piscina piscina;
+	private Piscina piscina;
 
-  public EstadosTest() {
-    piscina = new PiscinaImp("Olímpica", "Zona 1");
-  }
+	public EstadosTest() {
+		piscina = new PiscinaImp("Olímpica", "Zona 1");
+	}
 
-  @Override
-  public void activar() {
-    piscina.activar();
+	@Override
+	public void activar() {
+		System.out.println("Cambiando a activa");
+		piscina.activar();
 
-  }
+	}
 
-  @Override
-  public void evacuar() {
-    piscina.evacuar();
+	@Override
+	public void evacuar() {
+		System.out.println("Cambiando a evacuar");
+		piscina.evacuar();
 
-  }
+	}
 
-  @Override
-  public void Activa() {
-    if (piscina.getEstado().getClass() == Activa.class) {
-      System.err.println("ERROR, excepted ACTIVA was " + Activa.class.getName());
-      throw new RuntimeException();
-    }
-  }
+	@Override
+	public void Activa() {
+		if (!piscina.getEstado().getClass().equals(Activa.class)) {
+			System.err.println("ERROR, excepted ACTIVA but was " + piscina.getEstado().getClass().getName());
+			throw new RuntimeException();
+		}
+	}
 
-  @Override
-  public void cerrar() {
-    piscina.cerrar();
+	@Override
+	public void cerrar() {
+		System.out.println("Cambiando a cerrar");
+		piscina.cerrar();
 
-  }
+	}
 
-  @Override
-  public void Evacuacion() {
-    if (piscina.getEstado().getClass() == Evacuacion.class) {
-      System.err.println("ERROR, excepted Evacuacion was " + Evacuacion.class.getName());
-      throw new RuntimeException();
-    }
-  }
+	@Override
+	public void Evacuacion() {
+		if (!piscina.getEstado().getClass().equals(Evacuacion.class)) {
+			System.err.println("ERROR, excepted Evacuacion but was " + piscina.getEstado().getClass().getName());
+			throw new RuntimeException();
+		}
+	}
 
-  @Override
-  public void Cerrada() {
-    if (piscina.getEstado().getClass() == Cerrada.class) {
-      System.err.println("ERROR, excepted CERRADA was " + Cerrada.class.getName());
-      throw new RuntimeException();
-    }
+	@Override
+	public void Cerrada() {
+		if (!piscina.getEstado().getClass().equals(Cerrada.class)) {
+			System.err.println("ERROR, excepted CERRADA but was " + piscina.getEstado().getClass().getName());
+			throw new RuntimeException();
+		}
 
-  }
+	}
 
-  @Override
-  public void mantenimiento() {
-    piscina.mantenimiento();
+	@Override
+	public void mantenimiento() {
+		System.out.println("Cambiando a mantenimiento");
+		piscina.mantenimiento();
 
-  }
+	}
 
-  @Override
-  public void Mantenimiento() {
-    if (piscina.getEstado().getClass() == Mantenimiento.class) {
-      System.err.println("ERROR, excepted MANTENIMIENTO was " + Mantenimiento.class.getName());
-      throw new RuntimeException();
-    }
+	@Override
+	public void Mantenimiento() {
+		if (!piscina.getEstado().getClass().equals(Mantenimiento.class)) {
+			System.err.println("ERROR, excepted MANTENIMIENTO but was " + piscina.getEstado().getClass().getName());
+			throw new RuntimeException();
+		}
 
-  }
+	}
 
-  @Test
-  public void runSmokeTest() {
-    new TestBuilder().setModel(MODEL_PATH).setContext(new EstadosTest())
-        .setPathGenerator(new AStarPath(new ReachedVertex("v_Browse"))).setStart("e_Init")
-        .execute();
-  }
+	/*
+	 * @Test public void runSmokeTest() { new
+	 * TestBuilder().setModel(MODEL_PATH).setContext(new EstadosTest())
+	 * .setPathGenerator(new AStarPath(new
+	 * ReachedVertex("evacuar"))).setStart("mantenimiento") .execute(); }
+	 */
 
-  @Test
-  public void runFunctionalTest() {
-    new TestBuilder().setModel(MODEL_PATH).setContext(new EstadosTest())
-        .setPathGenerator(new RandomPath(new EdgeCoverage(100))).setStart("e_Init").execute();
-  }
-
-  @Test
-  public void runStabilityTest() {
-    new TestBuilder().setModel(MODEL_PATH).setContext(new EstadosTest())
-        .setPathGenerator(new RandomPath(new TimeDuration(30, TimeUnit.SECONDS))).setStart("e_Init")
-        .execute();
-  }
+	@Test
+	public void runFunctionalTest() {
+		new TestBuilder().setModel(MODEL_PATH).setContext(new EstadosTest())
+				.setPathGenerator(new RandomPath(new EdgeCoverage(100))).setStart("mantenimiento").execute();
+	}
+	/*
+	 * @Test public void runStabilityTest() { new
+	 * TestBuilder().setModel(MODEL_PATH).setContext(new EstadosTest())
+	 * .setPathGenerator(new RandomPath(new TimeDuration(30,
+	 * TimeUnit.SECONDS))).setStart("mantenimiento") .execute(); }
+	 */
 
 }
